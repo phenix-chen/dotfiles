1e814269fc0f5c203c92171d31b230dd531b0ce8
chen <c252156290@gmail.com>
2020-11-21 22:42:11 +0800

    add install zsh-completions

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,13 +25,15 @@ cp bootstrap.sh $BIN/
 
 # source init.sh
 touch ~/.zshrc
-gsed -i "\:$ETC/init.sh:d" ~/.zshrc
+sed -i "\:$ETC/init.sh:d" ~/.zshrc
 echo ". $ETC/init.sh" >> ~/.zshrc
 
 # vim
 touch ~/.vimrc
-gsed -i "\:$ETC/vimrc.vim:d" ~/.vimrc
+sed -i "\:$ETC/vimrc.vim:d" ~/.vimrc
 echo "source $ETC/vimrc.vim" >> ~/.vimrc
 
-
+if ! [ -d ~/.oh-my-zsh/custom/plugins/zsh-completions ]; then
+    git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+fi
 . ~/.zshrc
