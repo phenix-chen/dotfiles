6e6e14046748425b8b02c55e69a4c2bc667431f4
chen <c252156290@gmail.com>
2020-11-23 20:45:47 +0800

    delete the content of local.sh

--- etc/local.sh
+++
@@ -1,5 +0,0 @@
-#####################
-# Environment variable
-#####################
-# set privoxy
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" # export PYTHONPATH="/anaconda3/lib/python3.6/site-packages: /Users/chen/Dropbox/Source/python:$PYTHONPATH"
